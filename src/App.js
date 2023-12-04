7c1a6918873a04f1e7825808a4033e0cff598009
RajeshLM10 <95027139+RajeshLM10@users.noreply.github.com>
2023-12-04 12:52:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// App.js
+import React from 'react';
+import EmojiQuote from './EmojiQuote';
+import CurrencyQuote from './CurrencyQuote';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <CurrencyQuote></CurrencyQuote>
+      </header>
+    </div>
+  );
+}
+
+export default App;
